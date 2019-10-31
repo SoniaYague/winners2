6f79b638027f30fdebe8f2656a713fe942a9555a
SoniaYague <57186429+SoniaYague@users.noreply.github.com>
2019-10-31 12:24:29 +0100

    Add files via upload

---
+++ final.R
@@ -0,0 +1,2 @@
+library(shiny)
+runApp("C:/Sonia/EAE/Business Performance Analysis/trabajo/winners2")
---
+++ ui.R
@@ -0,0 +1,28 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
+
+# Use a fluid Bootstrap layout
+fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Coches en el mundo"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("region", "Region:", 
+                  choices=colnames(WorldPhones)),
+      hr(),
+      helpText("Datos de los coches en todo el mundo.")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("phonePlot")  
+    )
+    
+  )
+)
